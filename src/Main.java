d5fedcde3355f9bd7a54351a41c216f29c49b4eb
LKanan <lucienkanani@gmail.com>
2025-04-01 15:42:40 +0200

    Update Main to use a list of Animal and instantiate ChauveSouris

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-//        Mamifere mamifere = new Mamifere("Chien", 3, "Poils");
-//        Oiseau oiseau = new Oiseau("Aigle", 2, "1.5m");
-        ChauveSouris chauveSouris = new ChauveSouris("Batman", 1, "Poils");
-
-        chauveSouris.manger();
-        chauveSouris.dormir();
-
-//        oiseau.voler();
-        chauveSouris.allaiter();
-        chauveSouris.voler();
-        chauveSouris.chanter();
-
+        List<Animal> animaux = new ArrayList<>();
+        animaux.add(new ChauveSouris("ChauveSouris", 2, "Noir"));
+        animaux.add(new ChauveSouris("ChauveSouris", 2, "Noir"));
     }
 }
